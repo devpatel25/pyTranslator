21fdd02f5297a9285a346f46b99928bffdde6540
devpatel25 <devpatel25112001@gmail.com>
2024-01-02 15:09:30 +0530

    Update translator.py to handle errors when reading and writing files.

--- translator.py
+++ translator.py
@@ -5,12 +5,23 @@ import xlrd
 
 def translate_file(input_file,output_file):
     start_time=time.time()      #start the timer
-    df= pd.read_excel(input_file)
+    # Load the file
+    try:
+        df = pd.read_excel(input_file)
+    except Exception as e:
+        print(f"Error: {e}")
+        return
+
     translator=Translator()
     for col in df.columns:
-        df[col]=df[col].apply(lambda x: translator.tranlate(x).text)
+        df[col]=df[col].apply(lambda x: translator.translate(x).text if not translator.translate(x).text == "error" else x)
 
-    df.to_excel(output_file,index=False)
+    # Save the translated data to a new file
+    try:
+        df.to_excel(output_file, index=False)
+    except Exception as e:
+        print(f"Error: {e}")
+        return
 
     run_time = time.time() - start_time     #get the run time
     print(f"Translation and saving completed in {run_time: .2f} sec.")
